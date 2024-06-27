ea1121df24fb83a0f98febbe7d97a53374cc9644
NickKipshidze <kipshidze.nick@gmail.com>
2024-06-27 10:50:20 +0400

    Update decision boundary helper function

--- tensorflow-dots/helperfun.py
+++ tensorflow-dots/helperfun.py
@@ -1,21 +1,22 @@
 # Helper functions
 
-import torch
-import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_decision_boundary(model: tf.keras.Sequential, X: np.ndarray, y: np.ndarray):    
+def plot_decision_boundary(model, X, y):
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
-    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 101), np.linspace(y_min, y_max, 101))
+    xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
 
-    X_to_pred_on = np.column_stack((xx.ravel(), yy.ravel()))
-    y_logits = torch.from_numpy(model.predict(X_to_pred_on))
+    x_in = np.c_[xx.ravel(), yy.ravel()]
 
-    y_pred = y_logits.argmax(dim=1)
+    y_pred = model.predict(x_in)
 
-    y_pred = y_pred.reshape(xx.shape).detach().numpy()
+    if len(y_pred.shape) > 1 and y_pred.shape[1] > 1:
+        y_pred = np.argmax(y_pred, axis=1).reshape(xx.shape)
+    else:
+        y_pred = np.round(y_pred).reshape(xx.shape)
+
     plt.style.use("seaborn-v0_8-darkgrid")
     plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
     plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
