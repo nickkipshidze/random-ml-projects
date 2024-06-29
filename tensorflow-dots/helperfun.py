b01f872490d1e212895ecdd062e5dfc15add2029
NickKipshidze <kipshidze.nick@gmail.com>
2024-06-29 06:24:10 +0400

    Add alpha argument to the decision boundary function

--- tensorflow-dots/helperfun.py
+++ tensorflow-dots/helperfun.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_decision_boundary(model, X, y):
+def plot_decision_boundary(model, X, y, alpha=0.7):
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100), np.linspace(y_min, y_max, 100))
@@ -18,7 +18,7 @@ def plot_decision_boundary(model, X, y):
         y_pred = np.round(y_pred).reshape(xx.shape)
 
     plt.style.use("seaborn-v0_8-darkgrid")
-    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=0.7)
+    plt.contourf(xx, yy, y_pred, cmap=plt.cm.RdYlBu, alpha=alpha)
     plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.RdYlBu)
     plt.xlim(xx.min(), xx.max())
     plt.ylim(yy.min(), yy.max())
